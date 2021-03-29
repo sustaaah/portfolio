6097520828d98754de0084b36160046acce7e8fb
sustaaah <64201334+sustaaah@users.noreply.github.com>
2021-03-29 13:52:22 +0200

    saving

---
+++ content/js/script.js
@@ -0,0 +1,10 @@
+setTimeout(expireLoader, 3000)
+
+
+function expireLoader(){
+	// TODO amke animation opacity fade for loader
+}
+
+
+
+
--- content/js/scriptPrjct.js
+++
@@ -1,6 +0,0 @@
-function getPoint() {
-	var target = document.getElementById("target");
-	var output = document.getElementById("output");
-
-	output.innerHTML = "offsetTop: " + target.offsetLeft;
-}
