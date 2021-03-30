dc026e659b7d204e9c48105925c989a75662bf3f
sustaaah <64201334+sustaaah@users.noreply.github.com>
2021-03-30 23:44:56 +0200

    saving

--- content/js/script.js
+++ content/js/script.js
@@ -1,10 +1,21 @@
 setTimeout(expireLoader, 3000)
-
+setTimeout(function (){ 
+	var loaderContainer = document.querySelector('#linkLoader01');
+	loaderContainer.classList.add('linkHover'); }, 500 )
 
 function expireLoader(){
-	// TODO amke animation opacity fade for loader
+	// make animation opacity fade for loader
+	var loaderContainer = document.querySelector('#loaderContainer');
+	loaderContainer.classList.add('loaderExpire');
+
+	// TODO display app
+	var app = document.getElementById('app')
+	app.style.display = "block";
+
+
+
+	// TODO display none loader
+
+
+
 }
-
-
-
-
