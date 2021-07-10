b1fedba48fc18be65f8778eb43bf069d54c4b150
sustaaah <sustagiovannipaolo@gmail.com>
2021-07-10 20:12:32 +0200

    saving work

--- content/js/script.js
+++ content/js/script.js
@@ -18,3 +18,20 @@ function expireLoader(){
 	// TODO display none loader
 	//loaderContainer.style.display = "none";
 }
+
+function startTime() {
+	const today = new Date();
+	let h = today.getHours();
+	let m = today.getMinutes();
+	let s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('hour').innerHTML = h;
+	document.getElementById('minute').innerHTML = m;
+	setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
+}
